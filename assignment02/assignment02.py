2bf5ec1466d66d194bac0bffea0d772f7f3bf0e4
Lucas Machado <lcmachado.cs@gmail.com>
2023-04-22 17:24:12 -0300

    feat: testing low pass

--- assignment02/assignment02.py
+++ assignment02/assignment02.py
@@ -28,9 +28,63 @@ def root_mean_squared_error(reference_img, enhanced_img):
 
   return error
 
+def read_input():
+  """
+  Reads input from stdin and returns the following variables:
+    I: input image file path
+    H: expected image file path
+    i: filter index (0, 1, 2, 3, 4, 5, 6, 7 or 8)
+  """
+  input_image_path = str(input())
+  expect_image_path = str(input())
+  i = int(input())
+
+  # Remove trailing whitespaces from image paths 
+  input_image_path = input_image_path.strip()
+  expect_image_path = expect_image_path.strip()
+
+  I = imageio.imread(input_image_path)
+  H = imageio.imread(expect_image_path)
+
+  return I, H, i
 
 def main():
-  pass
+  I, H, i = read_input()
+
+  match i:
+    case 0:
+      # Ideal Low-pass filter
+      r = int(input())
+    case 1:
+      # Ideal High-pass filter
+      r = int(input())
+    case 2:
+      # Ideal Band-pass filter
+      r1 = int(input())
+      r2 = int(input())
+    case 3:
+      # Laplacian High-pass filter
+      pass
+    case 4:
+      # Gaussian Low-pass filter
+      sigma1 = int(input())
+      sigma2 = int(input())
+    case 5:
+      # Butterworth Low-pass filter
+      D0 = float(input())
+      n = float(input())
+    case 6:
+      # Butterworth High-pass filter
+      D0 = float(input())
+      n = float(input())
+    case 7:
+      # Butterworth Band-reject filter
+      pass
+    case 8:
+      # Butterworth Band-pass filter
+      pass
+
+  print(I.shape, H.shape, i)
 
 if __name__ == '__main__':
   main()
