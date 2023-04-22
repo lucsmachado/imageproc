a4cd605d6058c5d923bffe12c075fdf69dc0e81a
Lucas Machado <lcmachado.cs@gmail.com>
2023-04-22 19:55:12 -0300

    feat: solved for ideal filters

--- assignment02/assignment02.py
+++ assignment02/assignment02.py
@@ -9,7 +9,6 @@ Student name: Lucas Carvalho Machado
 USP number: 11208429
 """
 
-import os
 import numpy as np
 import imageio.v2 as imageio
 
@@ -27,6 +26,94 @@ def root_mean_squared_error(reference_img, enhanced_img):
   error = np.sqrt(sum/(M*N))
 
   return error
+
+def normalize(values, range=1):
+  """
+  Normalizes the given values to the inclusive range [0, range].
+  """
+  min = np.min(values)
+  max = np.max(values)
+  return (values - min) / (max - min) * range
+
+def apply_filter(fft, filter):
+  """
+  Applies the given filter to the given FFT, converts it back to the spatial domain and returns the result.
+  """
+  fft_filtered = np.multiply(fft, filter)
+
+  # Shift the zero-frequency component back to the top-left corner.
+  fft_filtered_unshift = np.fft.ifftshift(fft_filtered)
+
+  # Convert the filtered FFT back to the spatial domain, then take only the real part.
+  img_filtered = np.fft.ifft2(fft_filtered_unshift).real
+
+  # Normalize the filtered image to the range [0, 255].
+  img_normalized = normalize(img_filtered, 255)
+
+  return img_normalized
+
+def distance(p1, p2):
+  """
+  Computes then returns the Euclidean distance between two given points.
+  """
+  return np.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+
+def high_pass(shape, u, v, r):
+  """
+  Returns the value of the ideal high-pass filter at the given coordinates.
+  """
+  P, Q = shape
+
+  # Compute the distance between the current coordinates and the center of the image.
+  D = distance((u, v), (P/2, Q/2))
+
+  if D <= r:
+    # If the distance is less than or equal to the radius, the filter value is 0 (the frequency is blocked).
+    return 0
+  else:
+    # Otherwise, the filter value is 1 (the frequency is allowed).
+    return 1
+
+def low_pass(shape, u, v, r):
+  """
+  Returns the value of the ideal low-pass filter at the given coordinates.
+  """
+  P, Q = shape
+
+  # Compute the distance between the current coordinates and the center of the image.
+  D = distance((u, v), (P/2, Q/2))
+
+  if D <= r:
+    # If the distance is less than or equal to the radius, the filter value is 1 (the frequency is allowed).
+    return 1
+  else:
+    # Otherwise, the filter value is 0 (the frequency is blocked).
+    return 0
+
+def get_filter(shape, filter_fn, *args):
+  """
+  Returns a filter of the given shape using the given filter function.
+  """
+  P, Q = shape
+  filter = np.zeros((P, Q), dtype=np.float32)
+
+  # Loop over each pixel in the filter and compute its value.
+  for u in range(P):
+    for v in range(Q):
+      filter[u, v] = filter_fn(shape, u, v, *args)
+
+  return filter
+
+def get_fft(img):
+  """
+  Computes then returns the Fast Fourier Transform of the given image.
+  """
+  fft = np.fft.fft2(img)
+
+  # Shift the zero-frequency component to the center of the spectrum.
+  fft_shift = np.fft.fftshift(fft)
+
+  return fft_shift
 
 def read_input():
   """
@@ -55,9 +142,17 @@ def main():
     case 0:
       # Ideal Low-pass filter
       r = int(input())
+      F = get_fft(I)
+      P, Q = F.shape
+      filter = get_filter((P, Q), low_pass, r)
+      H_hat = apply_filter(F, filter)
     case 1:
       # Ideal High-pass filter
       r = int(input())
+      F = get_fft(I)
+      P, Q = F.shape
+      filter = get_filter((P, Q), high_pass, r)
+      H_hat = apply_filter(F, filter)
     case 2:
       # Ideal Band-pass filter
       r1 = int(input())
@@ -84,7 +179,9 @@ def main():
       # Butterworth Band-pass filter
       pass
 
-  print(I.shape, H.shape, i)
+  # Compute the Root Mean Squared Error (RMSE) between the expected image and the enhanced image, then print it with 4 decimal places.
+  error = root_mean_squared_error(H_hat, H)
+  print(f'{error:.4f}')
 
 if __name__ == '__main__':
   main()
